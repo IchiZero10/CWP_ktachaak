8283269e1ce5ec46ea746de08fe16f6f538d6e32
TKanidpat <66070245@kmitl.ac.th>
2026-09-24 15:20:41 +0700

    edit checkmate.py

--- discovery_piscine/rush/checkmate.py
+++ discovery_piscine/rush/checkmate.py
@@ -1,20 +1,28 @@
 def checkmate(board: str):
     # find king and turn to list
     king_x, king_y = -1, -1
-    arr = [i.strip() for i in board.splitlines()]
+    arr = [
+        i.strip() for i in board.splitlines()
+    ]
     board_size = len(arr)
     king_counter = sum(i.count("K") for i in arr)
-    if any(len(row) != board_size for row in arr):
+    if any(
+        len(row) != board_size for row in arr
+    ):
         print("Board is not square")
         return
-    if not king_counter:
+    if (
+        not king_counter
+    ):
         print("King not found")
         return
     elif king_counter > 1:
         print("Multiple King Found")
         return
-
-    for i, j in enumerate(arr):
+    # find king x y
+    for i, j in enumerate(
+        arr
+    ):
         finder = j.find("K")
         if finder != -1:
             king_y = i
@@ -23,26 +31,36 @@ def checkmate(board: str):
     # case Pawn
     # X.X
     # .P.
-    if king_y + 1 < board_size and (
-        (king_x - 1 >= 0 and arr[king_y + 1][king_x - 1] == "P")
-        or (king_x + 1 < board_size and arr[king_y + 1][king_x + 1] == "P")
+    if (
+        king_y + 1 < board_size
+        and (
+            (
+                king_x - 1 >= 0 and arr[king_y + 1][king_x - 1] == "P"
+            )
+            or (
+                king_x + 1 < board_size and arr[king_y + 1][king_x + 1] == "P"
+            )
+        )
     ):
         print("Success")
         return
     # horizontal and vertical (Queen and Rook)
-    for i in range(board_size):
-        if arr[i][king_x] in "QR" or arr[king_y][i] in "QR":
+    for i in range(board_size):  # วนเลข 0-ความยาวตาราง
+        if (
+            arr[i][king_x] in "QR" or arr[king_y][i] in "QR"
+        ):
             print("Success")
             return
     # diagonal
     for i in range(board_size):
-        if i + king_y - king_x < board_size:
-            if arr[i][i + king_y - king_x] in "QB":
+        y = i + king_y - king_x
+        if 0 <= y < board_size:
+            if arr[i][y] in "QB":
                 print("Success")
                 return
-
-        if -i + king_y + king_x < board_size:
-            if arr[i][-i + king_y + king_x] in "QB":
+        y = -i + king_y + king_x
+        if 0 <= y < board_size:
+            if arr[i][y] in "QB":
                 print("Success")
                 return
     print("Failed")
